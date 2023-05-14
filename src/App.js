edbfe0367c2f6e4f877ae2265f1ba1c90a09827d
Shoppedemo <tuananhdtu97@gmail.com>
2023-05-15 04:16:03 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Nav from './views/Nav/Nav';
+import './views/Nav/Nav.scss'
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+// const App =() => {
+//   let [name, setName] = useState('')
+//   let [title, setTitle] = useState('')
+
+//   const handleEventClick = () => {
+//     setName(title)
+//   }
+//   const handleOnChangleInput = (event) => {
+//     setTitle(event.target.value)
+//   }
+  
+//   return (
+//     <div className="App">
+//       <Nav/>
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>You can input Name : {name}</h1>
+//         <input type='text' value={title} onChange={(event)=> handleOnChangleInput(event)}/>
+//         <button type='button' onClick={() => handleEventClick()}>Click me</button>
+//       </header>
+//     </div>
+    
+//   );
+// }
+
+// export default App;
+
+
+const App = () => {
+  let [name, setName] = useState('')
+  let [title, setTitle] = useState('')
+  const hanleOnChangeTitle = (event) => {
+    console.log(event.target.value)
+    setTitle(event.target.value)
+  }
+  const hanleOnAdd = () => {
+    setName(title)
+  }
+  return(
+    <div className='App'>
+      <Nav/>
+      <header className='App-header'>
+      <h1>You can input Name : {name}</h1>
+      <input type='text' onChange={(event) => hanleOnChangeTitle(event)}/>
+      <button type='button' onClick={() => hanleOnAdd()}>Add</button>
       </header>
     </div>
   );
 }
-
-export default App;
+export default App
